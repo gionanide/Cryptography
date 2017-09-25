348962fe60c2daebfc81bc42f528df4d2a1da354
Manos Gionanidis <gionanidis32@gmail.com>
2017-09-25 13:46:52 +0200

    Update FeistelCipher.py

--- FeistelCipher.py
+++ FeistelCipher.py
@@ -132,7 +132,122 @@ mainProcedure()
 	
 
 
-	
+                                                    DECRYPTION
+		
+		
+import hashlib
+import struct
+
+
+def get_passw():
+    password = raw_input('Give me a password: ')
+    firstPass = hashlib.sha256(password.encode()).hexdigest()
+    secondPass = hashlib.sha256(firstPass.encode()).hexdigest()
+    print "LEN HASH_PSW",len(firstPass+secondPass)
+    return firstPass+secondPass
+
+
+def XOR(op1, op2):
+    # compute op1 ^ op2 must to be strings of format hexa
+    a=int(op1,16)
+    b=int(op2,16)
+    return hex(a^b)[2:]
+
+def do_round(left, right,key):
+    tmp=right
+    right=XOR(left,f(key,right))
+    left=tmp
+    return left,right
+
+
+def encipher_block(plaintext_block,ks):
+    #encryption algo
+    plaintext_block=str2hex(plaintext_block)
+    print "str2hex of plaintext_block", plaintext_block
+    left=plaintext_block[:len(plaintext_block)/2]
+    right=plaintext_block[len(plaintext_block)/2:]
+    for i in range(len(ks)):
+        left ,right=do_round(left,right,ks[i])
+    return right+left
+
+def do_round_decr(left,right,key):
+    tmp=left
+    left=XOR(right,f(key,left))
+    right=tmp
+    return left,right
+
+
+def decipher_block(cipher_block,ks):
+    cipher_block=str2hex(cipher_block)
+    left=cipher_block[:len(cipher_block)/2] #this is actually Rn+1
+    right=cipher_block[len(cipher_block)/2:] #this is actually Ln+1
+    for k in reversed(ks):
+        left,right= do_round_decr(left,right,k)
+    return left+right
+
+def blockify(plaintext,blocksize):
+    #split text in block of blocksize byte
+    lenght=len(plaintext)
+    padding=0
+    if lenght % blocksize != 0 :
+        padding=blocksize - (lenght % blocksize)
+    print "PLAIN LEN: ",len(plaintext)
+    plaintext+=padding*" "
+    print "PLAIN+PADDING LEN: ",len(plaintext)
+    plaintext=map(''.join, zip(*[iter(plaintext)]*blocksize))
+    return plaintext
+
+def str2hex(string):
+    hexa=""
+    for i in string:
+        hexa+=hex(ord(i))[2:]
+    return hexa #return just the hexa string without 0x
+
+def hex2str(hex_string):
+    if len(hex_string)%2 == 0:
+        pass
+    else :
+        hex_string="0"+hex_string
+    return hex_string.decode("hex")
+
+def f(key,oper):
+    return key
+
+def build_keys_stream(password):
+    KS=map(''.join, zip(*[iter(password)]*8))
+    for i in range(len(KS)):
+        KS[i]="0x"+KS[i]
+    return KS
+
+def main_routine(plaintext):
+    passw=get_passw()
+    KS=build_keys_stream(passw)
+    plaintext_blocked=blockify(plaintext,8)
+    cipher=""
+    for block in plaintext_blocked:
+        cipher+=encipher_block(block,KS)
+    cipher=hex2str(cipher)
+    print"CIPHER", cipher
+
+    ciphertext_blocked=blockify(cipher,8)
+    res=""
+    for cblock in reversed(ciphertext_blocked):
+        res=hex2str(decipher_block(cblock,KS))+res
+    print"\ndecription:", res, " len: ", len(res)
+
+
+#main
+#main_routine('Hello my name is Manos')
+
+passw=get_passw()
+KS=build_keys_stream(passw)
+res=""
+fi=open("/home/manos/fei.Feistel","rb").read()
+for cblock in reversed(blockify(fi,8)):
+    res=hex2str(decipher_block(cblock,KS))+res
+print "RES: ",res
+#
+
 	
 	
 	
